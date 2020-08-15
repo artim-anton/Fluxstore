e291677c817166c57c5f0c9f21ed3cc2017e87de
artim-anton <artim-anton@yandex.ru>
2020-08-15 13:06:12 +0300

    Initial commit, create ViewPager

---
+++ app/src/main/java/com/artimanton/fluxstore/fragments/PageFragment3.java
@@ -0,0 +1,27 @@
+package com.artimanton.fluxstore.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.artimanton.fluxstore.R;
+
+public class PageFragment3 extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup)inflater.
+                inflate(R.layout.page_3,container,
+                        false);
+        return rootView;
+
+    }
+}
